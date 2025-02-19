d5b3f94b83a7843ce16b063c1f0179232ff4fdbd
Aleksandr Baranov <alexmaksimum54@yandex.ru>
2025-02-19 21:25:13 +0700

    Тесты на ручку "Создание курьера" готовы

--- src/test/java/CreatingCourierTest.java
+++ src/test/java/CreatingCourierTest.java
@@ -1,3 +1,6 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.After;
@@ -14,38 +17,79 @@ public class CreatingCourierTest {
     }
 
     @Test
-    public void returnsTheCorrectBody(){
-        CourierAuthorizationData json = new CourierAuthorizationData("Taliban", "4336", "saske");
-
-        Response response = given()
-                .header("Content-type", "application/json")
-                .body(json)
-                .when()
-                .post("/api/v1/courier");
-
+    @DisplayName("Returns the correct body and statusCode")
+    @Description("Возвращается корректное тело ответа и статус код при успешном запросе")
+    public void returnsTheCorrectBodyTest(){
+        Response response = creatingCourier();
         response.then().assertThat().body("ok", equalTo(true))
                 .and()
                 .statusCode(201);
     }
 
-    @After
-    public  void deleteCourier(){
-        CourierGetLoginData json = new CourierGetLoginData("Taliban", "4336");
+    @Test
+    @DisplayName("Creating identical couriers")
+    @Description("Должна вернуться ошибка \"409 conflict\" , тело ответа \"Этот логин уже используется\"")
+    public void identialCouriersTest(){
+        creatingCourier();
+        Response secondCourier = creatingCourier();
+        secondCourier.then().assertThat().body("message", equalTo("Этот логин уже используется"))
+                .and()
+                .statusCode(409);
+    }
 
+    @Test
+    @DisplayName("Bad request, no one field")
+    @Description("Возвращается ошибка \"400 Bad request\" при отсутствии одного из обязательных полей в теле запроса")
+    public void courierTwoFieldsBodyTest(){
+        Response response = creatingCourierTwoFields();
+        response.then().assertThat().body("message",equalTo("Недостаточно данных для создания учетной записи"))
+                .and()
+                .statusCode(400);
+    }
+
+    @Step("Передаём два поля в теле запроса вместо трёх обязательных")
+    public Response creatingCourierTwoFields(){
+        CourierAuthorizationDataNoField json = new CourierAuthorizationDataNoField("Taliban","saske");
+        Response response = given()
+                .header("Content-type", "application/json")
+                .body(json)
+                .when()
+                .post("/api/v1/courier");
+        return response;
+    }
+
+    @Step("Создание курьера")
+    public Response creatingCourier(){
+        CourierAuthorizationData json = new CourierAuthorizationData("Taliban", "4336", "saske");
+        Response response = given()
+                .header("Content-type", "application/json")
+                .body(json)
+                .when()
+                .post("/api/v1/courier");
+        return response;
+    }
+
+    @Step("Получаем ID созданного курьера")
+    public int getIdCourier(){
+        CourierGetLoginData json = new CourierGetLoginData("Taliban","4336");
         CouriersLogin loginID = given()
                 .header("Content-type", "application/json")
                 .body(json)
                 .post("/api/v1/courier/login")
-                .body()
                 .as(CouriersLogin.class);
+        return loginID.getId();
+    }
 
+    @Step("Удаление курьера по его ID")
+    public void deleteIdCourier(){
         Response response = given()
-                .header("Content-type", "application/json")
+                .header("Conrent-type", "application/json")
                 .when()
-                .delete("/api/v1/courier/" + loginID.getId());
+                .delete("/api/v1/courier/" + getIdCourier());
+    }
 
-        response.then().assertThat().body("ok", equalTo(true))
-                .and()
-                .statusCode(200);
+    @After
+    public void tearDown(){
+        deleteIdCourier();
     }
 }
