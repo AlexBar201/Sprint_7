d891977379eb28c62743b7c4f5968f35ff4f5af7
Aleksandr Baranov <alexmaksimum54@yandex.ru>
2025-02-22 12:08:10 +0700

    Тесты на ручку "Создание заказа" готовы

---
+++ src/main/java/NumberTrackOrder.java
@@ -0,0 +1,11 @@
+public class NumberTrackOrder {
+    private int track;
+
+    public int getTrack() {
+        return track;
+    }
+
+    public void setTrack(int track) {
+        this.track = track;
+    }
+}
